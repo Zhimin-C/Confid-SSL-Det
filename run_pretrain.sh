49bb2d6d3f3aa7d233c2873e1bcf59e683c16774
Zhimin-C <35618385+Zhimin-C@users.noreply.github.com>
2023-04-03 11:16:37 -0400

    Update run_pretrain.sh

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -4,4 +4,4 @@ DATASET=$3
 LABELED_LIST=$4
 mkdir -p "${LOG_DIR}"
 python -u pretrain.py --log_dir="${LOG_DIR}" --dataset="${DATASET}" \
---labeled_sample_list="${LABELED_LIST}" 2>&1|tee "${LOG_DIR}"/LOG_ALL.log &
+--labeled_sample_list="${LABELED_LIST}" --checkpoint_path="${CHECKPOINT_PATH}" 2>&1|tee "${LOG_DIR}"/LOG_ALL.log &
